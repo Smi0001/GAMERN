0854e7ebe1deaf11a64433704446f6522ccba357
shammi.hans <shammi.hans@coverfoxmail.com>
2020-03-14 04:19:11 +0530

    --removed unnecessary themes, different themes for diffenentiating both editors are still in use

--- client/src/constants/constants.js
+++ client/src/constants/constants.js
@@ -1,7 +1,4 @@
 import { defaultTheme, defaultTheme2 } from './tui-default-theme'
-import whiteTheme from './tui-white-theme'
-import blackTheme from './tui-black-theme'
-
 
 export const BYTES_TO_MB_DIVIDER = 1000000
 export const INTENTIONAL_NULL_VALUE = null // unintentional vallue is set as undefined
@@ -12,8 +9,6 @@ export const XTRA = 'xtra'
 export const imageEditorConfig = {
     defaultTheme,
     defaultTheme2,
-    blackTheme,
-    whiteTheme,
     imageDefaultName: 'image',
     menuFeatures: ['crop', 'flip', 'rotate', 'draw', 'shape', 'text', 'filter'],
     initMenu: '',
